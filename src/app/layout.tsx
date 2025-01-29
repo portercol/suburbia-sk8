611334f0a9e093b2a4eab7de5f8ff0bad1b92e4a
Collin <cporter.dev@gmail.com>
2025-01-28 22:24:09 -0700

    added button, header, heading, logo & svgfilters components & added the hero logos for desktop & mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,15 +1,22 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Bowlby_One_SC, DM_Mono } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import "./globals.css";
+import { Header } from '../components/Header';
+import { SVGFilters } from "@/components/SVGFilters";
+
+const bowlby = Bowlby_One_SC({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-bowlby-sc",
+  weight: "400",
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const dmMono = DM_Mono({
   subsets: ["latin"],
+  display: "swap",
+  variable: "--font-dm-mono",
+  weight: "500",
 });
 
 export const metadata: Metadata = {
@@ -25,8 +32,12 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${bowlby.variable} ${dmMono.variable} antialiased font-mono font-medium text-zinc-800`}
       >
+        <main>
+          <Header />
+        </main>
+        <SVGFilters />
         {children}
       </body>
     </html>
