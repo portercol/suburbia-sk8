49e0e3639b2ece4eaca56f0624356eaa57d5283a
Collin <cporter.dev@gmail.com>
2025-01-29 22:18:52 -0700

    added feature to make textAndImage slices sticky and sperated by 2 rem from eachother at the top

--- src/slices/TextAndImage/index.tsx
+++ src/slices/TextAndImage/index.tsx
@@ -12,6 +12,12 @@ import { Heading } from "@/components/Heading";
 import { ButtonLink } from "@/components/ButtonLink";
 import { Bounded } from "@/components/Bounded";
 import { ParallaxImage } from "./ParallaxImage";
+
+declare module "react" {
+  interface CSSProperties {
+    "--index"?: number;
+  }
+}
 /**
  * Props for `TextAndImage`.
  */
@@ -20,7 +26,7 @@ export type TextAndImageProps = SliceComponentProps<Content.TextAndImageSlice>;
 /**
  * Component for "TextAndImage" Slices.
  */
-const TextAndImage = ({ slice }: TextAndImageProps): JSX.Element => {
+const TextAndImage = ({ slice, index }: TextAndImageProps): JSX.Element => {
   const theme = slice.primary.theme;
 
   return (
@@ -28,11 +34,13 @@ const TextAndImage = ({ slice }: TextAndImageProps): JSX.Element => {
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
       className={clsx(
+        "sticky top-[calc(var(--index)*2rem)]",
         theme === "Blue" && "bg-texture bg-brand-blue text-white",
         theme === "Orange" && "bg-texture bg-brand-orange text-white",
         theme === "Navy" && "bg-texture bg-brand-navy text-white",
         theme === "Lime" && "bg-texture bg-brand-lime text-white"
       )}
+      style={{ "--index": index}}
     >
       <div className="grid grid-cols-1 items-center gap-12 md:grid-cols-2 md:gap-24">
         <div
