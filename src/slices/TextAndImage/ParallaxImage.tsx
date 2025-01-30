9f6779d2268758ab6062fb37c72000cc9b535b61
Collin <cporter.dev@gmail.com>
2025-01-29 21:50:08 -0700

    added logic for parallax images – created percentages for the translate style on both background & foreground images

--- src/slices/TextAndImage/ParallaxImage.tsx
+++ src/slices/TextAndImage/ParallaxImage.tsx
@@ -1,7 +1,9 @@
+'use client'
+
 import { ImageField } from "@prismicio/client";
 import { PrismicNextImage } from "@prismicio/next";
 import clsx from "clsx";
-import React from "react";
+import React, { useEffect, useRef } from "react";
 
 type Props = {
   foregroundImage: ImageField;
@@ -14,13 +16,65 @@ export function ParallaxImage({
   backgroundImage,
   className,
 }: Props) {
+
+    const backgroundRef = useRef<HTMLDivElement>(null);
+    const foregroundRef = useRef<HTMLDivElement>(null);
+
+    const targetPosition = useRef({x:0, y:0});
+    const currentPosition = useRef({x:0, y:0});
+
+    useEffect(() => {
+        const frameId = requestAnimationFrame(animationFrame)
+        window.addEventListener("mousemove", onMouseMove)
+
+        function onMouseMove(event: MouseEvent)  {
+            const {innerWidth, innerHeight} = window;
+
+            const xPercent = (event.clientX / innerWidth - 0.5) * 2
+            const yPercent = (event.clientY / innerHeight - 0.5) * 2
+
+            targetPosition.current = {
+                x: xPercent * -20,
+                y: yPercent * -20,
+            }            
+        }
+
+        function animationFrame() {
+            const { x: targetX, y: targetY } = targetPosition.current;
+            const { x: currentX, y: currentY } = currentPosition.current;
+        
+            const newX = currentX  + (targetX - currentX) * .1;
+            const newY = currentY  + (targetY - currentY) * .1;
+
+            currentPosition.current = {x: newX, y: newY};
+
+            if (backgroundRef.current) {
+                backgroundRef.current.style.transform = `translate(${newX}px, ${newY}px)`;
+            }
+            if (foregroundRef.current) {
+                foregroundRef.current.style.transform = `translate(${newX * 2.5}px, ${newY * 2.5}px)`;
+            }
+
+            requestAnimationFrame(animationFrame)
+        }
+
+        return () => {
+            window.removeEventListener("mousemove", onMouseMove);
+            cancelAnimationFrame(frameId);
+        }
+    }, [])
+
   return (
     <div className={clsx("grid grid-cols-1 place-items-center", className)}>
-      <div className="col-start-1 row-start-1 transition-transform">
-        <PrismicNextImage field={backgroundImage} />
+      <div ref={backgroundRef} className="col-start-1 row-start-1 transition-transform">
+        <PrismicNextImage field={backgroundImage} className="w-11/12" />
       </div>
-      <div className="col-start-1 row-start-1 transition-transform">
-        <PrismicNextImage field={foregroundImage} />
+      <div ref={foregroundRef} className="col-start-1 row-start-1 transition-transform h-full w-full place-items-center">
+        <PrismicNextImage
+          field={foregroundImage}
+          className="h-full max-h-[500px] w-auto"
+          imgixParams={{ height: 600 }}
+        />
       </div>
     </div>
   );
