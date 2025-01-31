f35917a101af42801e76c6eecbe92b2bac3d3df0
Collin <cporter.dev@gmail.com>
2025-01-30 22:42:00 -0700

    added footerPhysics component w/ matterJS logic & updated footer to hold skateboards

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,16 +4,22 @@ import React from "react";
 
 import { Logo } from "@/components/Logo";
 import { Bounded } from "./Bounded";
+import { asImageSrc } from "@prismicio/client";
+import { FooterPhysics } from "./FooterPhysics";
 
 type Props = {};
 
 export async function Footer({}: Props) {
   const client = createClient();
   const settings = await client.getSingle("settings");
+  const boardTextureURLs = settings.data.footer_skateboards
+    .map((item) => asImageSrc(item.skateboard, { h: 600 }))
+    .filter((url): url is string => Boolean(url));
 
   return (
     <footer className="bg-texture bg-zinc-900 text-white overflow-hidden">
       <div className="relative h-[75vh] ~p-10/16 md:aspect-auto">
+        {/* Image */}
         <PrismicNextImage
           field={settings.data.footer_image}
           alt=""
@@ -21,21 +27,25 @@ export async function Footer({}: Props) {
           className="object-cover"
           width={1200}
         />
-        {/* Image */}
         {/* Physics Boards */}
+        <FooterPhysics
+          boardTextureURLs={boardTextureURLs}
+          className="absolute inset-0 overflow-hidden"
+        />
         {/* Logo */}
         <Logo className="pointer-events-none relative h-20 mix-blend-exclusion md:h-28" />
       </div>
+
+      {/* List of Links */}
       <Bounded as="nav">
         <ul className="flex flex-wrap justify-center gap-8 ~text-lg/xl">
-           {settings.data.navigation.map((item) => (
+          {settings.data.navigation.map((item) => (
             <li key={item.link.text} className="hover:underline">
-                <PrismicNextLink field={item.link} />
+              <PrismicNextLink field={item.link} />
             </li>
-           ))} 
+          ))}
         </ul>
       </Bounded>
-      {/* List of Links */}
     </footer>
   );
 }
