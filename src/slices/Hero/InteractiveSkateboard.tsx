e5e45559a3003105775d88ef243239763ff09e12
Collin <cporter.dev@gmail.com>
2025-02-05 22:47:13 -0700

    added skateboard component textures for deck, wheels, trucks and bolts using three.js as well as added props across necessary files

--- src/slices/Hero/InteractiveSkateboard.tsx
+++ src/slices/Hero/InteractiveSkateboard.tsx
@@ -5,20 +5,43 @@ import { ContactShadows, Environment, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 
-type Props = {};
+type Props = {
+  deckTextureURL: string;
+  wheelTextureURL: string;
+  truckColor: string;
+  boltColor: string;
+};
 
-function Scene() {
-    return (
-      <group>
-        <OrbitControls />
-        <Environment files={"/hdr/warehouse-256.hdr"} />
-        <Skateboard />
-        <ContactShadows opacity={.6} position={[0, -.2, 0]} />
-      </group>
-    );
-  }
+function Scene({
+  deckTextureURL,
+  wheelTextureURL,
+  truckColor,
+  boltColor,
+}: Props) {
+  return (
+    <group>
+      <OrbitControls />
+      <Environment files={"/hdr/warehouse-256.hdr"} />
+      <Skateboard
+        wheelTextureURLs={[wheelTextureURL]}
+        wheelTextureURL={wheelTextureURL}
+        deckTextureURLs={[deckTextureURL]}
+        deckTextureURL={deckTextureURL}
+        truckColor={truckColor}
+        boltColor={boltColor}
+        constantWheelSpin
+      />
+      <ContactShadows opacity={0.6} position={[0, -0.2, 0]} />
+    </group>
+  );
+}
 
-export function InteractiveSkateboard({}: Props) {
+export function InteractiveSkateboard({
+  deckTextureURL,
+  wheelTextureURL,
+  truckColor,
+  boltColor,
+}: Props) {
   return (
     <div className="absolute inset-0 z-10 flex items-center justify-center">
       <Canvas
@@ -26,11 +49,14 @@ export function InteractiveSkateboard({}: Props) {
         camera={{ position: [1.5, 1, 1.4], fov: 55 }}
       >
         <Suspense>
-          <Scene />
+          <Scene
+            deckTextureURL={deckTextureURL}
+            wheelTextureURL={wheelTextureURL}
+            truckColor={truckColor}
+            boltColor={boltColor}
+          />
         </Suspense>
       </Canvas>
     </div>
   );
 }
-
-
--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,4 +1,4 @@
-import { Content } from "@prismicio/client";
+import { asImageSrc, Content } from "@prismicio/client";
 import {
   PrismicRichText,
   PrismicText,
@@ -12,6 +12,11 @@ import { ButtonLink } from "@/components/ButtonLink";
 import { WideLogo } from "./WideLogo";
 import { TallLogo } from "./TallLogo";
 import { InteractiveSkateboard } from "./InteractiveSkateboard";
+
+const DEFAULT_DECK_TEXTURE = "/skateboard/Deck.webp";
+const DEFAULT_WHEEL_TEXTURE = "/skateboard/SkateWheel1.png";
+const DEFAULT_TRUCK_COLOR = "#6F6E6A";
+const DEFAULT_BOLT_COLOR = "#6F6E6A";
 /**
  * Props for `Hero`.
  */
@@ -21,6 +26,14 @@ export type HeroProps = SliceComponentProps<Content.HeroSlice>;
  * Component for "Hero" Slices.
  */
 const Hero = ({ slice }: HeroProps): JSX.Element => {
+  const deckTextureURL =
+    asImageSrc(slice.primary.skateboard_deck_texture) || DEFAULT_DECK_TEXTURE;
+  const wheelTextureURL =
+    asImageSrc(slice.primary.skateboard_wheel_texture) || DEFAULT_WHEEL_TEXTURE;
+  const truckColor =
+    slice.primary.skateboard_truck_color || DEFAULT_TRUCK_COLOR;
+  const boltColor = slice.primary.skateboard_bolt_color || DEFAULT_BOLT_COLOR;
+
   return (
     <Bounded
       data-slice-type={slice.slice_type}
@@ -50,7 +63,12 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
         </div>
       </div>
 
-      <InteractiveSkateboard />
+      <InteractiveSkateboard 
+        deckTextureURL={deckTextureURL}
+        wheelTextureURL={wheelTextureURL}
+        truckColor={truckColor}
+        boltColor={boltColor}
+      />
     </Bounded>
   );
 };
